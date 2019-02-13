eb4b3727c720e0689c0970a2cb5c7cf9442554dc
Madhuri Chalamangalam <madhuri.chalamangalam@gmail.com>
2019-02-13 03:02:54 +0000

    Added a POST Route to Receive the Form Submission

--- express_server.js
+++ express_server.js
@@ -9,6 +9,9 @@ var urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -22,16 +25,25 @@ app.get("/urls", (req, res) => {
     res.render("urls_index", templateVars);
   });
 
-  app.get("/urls/:shortURL", (req, res) => {
-
+app.get("/urls/new", (req, res) => {
+    res.render("urls_new");
+  });
+  
+app.get("/urls/:shortURL", (req, res) => {
     let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
     res.render("urls_show", templateVars);
   });
 
+
 app.get("/hello", (req, res) => {
     res.send("<html><body>Hello <b>World</b></body></html>\n");
   });
-  
+
+app.post("/urls", (req, res) => {
+    console.log(req.body);  // Log the POST request body to the console
+    res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  });
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
